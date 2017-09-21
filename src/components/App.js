5568b27373054700505aff440ab0ee4ffd1bf05a
Chad Yates <cayates1989@gmail.com>
2017-09-21 15:19:54 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Chad's Music Player</h2>
+        </div>
+        <p className="App-intro">
+          Store all your favorite tunes here.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
