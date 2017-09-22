51fb25868a7e573c4ec26d780a00bb358a29e3ee
Chad Yates <cayates1989@gmail.com>
2017-09-22 13:16:42 -0400

    had to change the button on my playlist form to be an onClick instead of an onsubmit

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -32,7 +32,7 @@ export default class PlayList extends Component{
     render(){
         return(
             <div className = "playList">
-                <input className = "submitButton" type="submit" value="Update List" onSubmit={this.fetchData}/>
+                <input className = "submitButton" type="submit" value="Update List" onClick={this.fetchData}/>
                 {this.state.songs.map((songInfo)=>{
                     return(
                         <PlayListItem 
